76e866ea1744f7deaff25ef4b316f2d3566b629e
hylmar <hylmar@aag.cz>
2019-07-10 09:23:58 +0200

    pdfgen

--- book/vagrant/asciidoc.cmd
+++ book/vagrant/asciidoc.cmd
@@ -1,1 +1,1 @@
-vagrant ssh -c "/usr/local/rvm/gems/default/gems/asciidoctor-pdf-1.5.0.alpha.18/bin/asciidoctor-pdf -a pdf-style=/opt/dev/themes/basic-theme.yml -a pdf-fontsdir=/opt/dev/fonts -r asciidoctor-mathematical /opt/dev/book.adoc"
+vagrant ssh -c "/usr/local/rvm/gems/default/gems/asciidoctor-pdf-1.5.0.beta.1/bin/asciidoctor-pdf -a pdf-style=/opt/dev/themes/basic-theme.yml -a pdf-fontsdir=/opt/dev/fonts -r asciidoctor-mathematical /opt/dev/book.adoc"
